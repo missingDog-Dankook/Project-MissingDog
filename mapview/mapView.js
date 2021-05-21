ef338e9c0180b1e70a0bf897170171b05e736b8e
ihatelethargy <naturalflavor1007@naver.com>
2021-05-21 15:09:23 +0900

    left-bar

--- mapview/mapView.js
+++ mapview/mapView.js
@@ -5,24 +5,29 @@ const getValueInput = () => {
   console.log(mapVal)
   initMap()
   function initMap() {
-    var options = {
-      zoom: 13,
-      center: { lat: 37.2433246, lng: 127.1569772 },
+    if (mapVal === '용인시 동물보호센터') {
+      var options = {
+        zoom: 13,
+        center: { lat: 37.2433246, lng: 127.1569772 },
+      }
+
+      var map = new google.maps.Map(document.getElementById('map'), options)
+
+      var marker = new google.maps.Marker({
+        position: { lat: 37.2433246, lng: 127.1569772 },
+        map: map,
+      })
+
+      var infoWindow = new google.maps.InfoWindow({
+        content: '<h1>용인시 동물보호센터</h1>',
+      })
+
+      marker.addListener('click', function () {
+        infoWindow.open(map, marker)
+      })
+    } else {
+      document.getElementById('valueInput').innerHTML =
+        inputValue + '를 찾을 수 없습니다.'
     }
-
-    var map = new google.maps.Map(document.getElementById('map'), options)
-
-    var marker = new google.maps.Marker({
-      position: { lat: 37.2433246, lng: 127.1569772 },
-      map: map,
-    })
-
-    var infoWindow = new google.maps.InfoWindow({
-      content: '<h1>용인시 동물보호센터</h1>',
-    })
-
-    marker.addListener('click', function () {
-      infoWindow.open(map, marker)
-    })
   }
 }
