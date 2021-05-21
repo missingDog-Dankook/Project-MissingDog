8e2e047c0e4b17bc77891d7fa6d7fbd21fb3319d
ihatelethargy <naturalflavor1007@naver.com>
2021-05-21 11:34:06 +0900

    mapview search

--- mapview/mapView.js
+++ mapview/mapView.js
@@ -1,24 +1,28 @@
-function initMap(){
-
+const getValueInput = () => {
+  let inputValue = document.getElementById('domTextElement').value
+  document.getElementById('valueInput').innerHTML = inputValue
+  var mapVal = inputValue
+  console.log(mapVal)
+  initMap()
+  function initMap() {
     var options = {
-      zoom : 13,
-      center:{lat : 37.2433246, lng: 127.1569772}
+      zoom: 13,
+      center: { lat: 37.2433246, lng: 127.1569772 },
     }
-    
-    var map = new google.maps.Map(document.getElementById('map'), options);
-  
-    var marker = new google.maps.Marker({
-      position:{lat:37.2433246, lng:127.1569772},
-      map:map,
-      
 
-    });
+    var map = new google.maps.Map(document.getElementById('map'), options)
+
+    var marker = new google.maps.Marker({
+      position: { lat: 37.2433246, lng: 127.1569772 },
+      map: map,
+    })
 
     var infoWindow = new google.maps.InfoWindow({
-      content:'<h1>용인시 동물보호센터</h1>'
-    });
+      content: '<h1>용인시 동물보호센터</h1>',
+    })
 
-    marker.addListener('click', function(){
-      infoWindow.open(map, marker);
-    });
+    marker.addListener('click', function () {
+      infoWindow.open(map, marker)
+    })
   }
+}
